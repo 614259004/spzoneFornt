a5a38e78db515da487800d130dc22622d3bfb6bf
614259004 <614259004@webmail.npru.ac.th>
2021-03-12 17:22:05 +0700

    Add New From Product

--- spzone-app-react/src/pages/ManageProduct.js
+++ spzone-app-react/src/pages/ManageProduct.js
@@ -7,17 +7,16 @@ import * as AiIcons from "react-icons/ai";
 import InputImage from '../UploadImageCustomer.js'
 
 const ManageProduct = () => {
-    const [showAdd,setShowAdd] = useState(false);
     return (
         <div className="brand-body-page">
             <div className="Head-brand">
                 <h1>สินค้า</h1>
                 <div className="Product-add-button">
                     
-                    <a onClick={() => {setShowAdd(true);}}><AiIcons.AiOutlinePlusCircle />เพิ่มสินค้า</a>
+                    <a ><AiIcons.AiOutlinePlusCircle />เพิ่มสินค้า</a>
                 </div>
             </div>
-            {!showAdd ?
+
             <div className="Product-card-layout">
             {ProductData.map((item, index)=>{
                 return(
@@ -28,6 +27,24 @@ const ManageProduct = () => {
                                 <div className="Product-name-group">
                                     <h5>{item.ProductName}</h5>
                                     <h6>{item.ProductId}</h6>
+                                    <div className="Size-layout">
+                                        <div className="size-product">
+                                            <h6>s</h6>
+                                            <h6>10ตัว</h6>
+                                        </div>
+                                        <div className="size-product">
+                                            <h6>m</h6>
+                                            <h6>10ตัว</h6>
+                                        </div>
+                                        <div className="size-product">
+                                            <h6>l</h6>
+                                            <h6>10ตัว</h6>
+                                        </div>
+                                        <div className="size-product">
+                                            <h6>xl</h6>
+                                            <h6>10ตัว</h6>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div className="button-brand-group">
                                     <a  className="Brand-pen"><AiIcons.AiFillEye/>รายละเอียด</a>
@@ -38,76 +55,10 @@ const ManageProduct = () => {
              )
             })} 
             </div>
-
-            :
-
-                <div className="Card-Add-Product">
-                    <Form >
-                        <h1>เพิ่มสินค้า</h1>
-                        <div className="form-input-add-product">
-
-                            <div className="page-add-product">
-                                <div className="input-add-product">
-                                    <label>ชื่อสินค้า</label>
-                                    <Input />
-                                </div>
-                                <div className="input-add-product">
-                                    <label>ไซต์</label>
-                                    <Input />
-                                </div>
-                                <div className="input-add-product">
-                                    <label>จำนวน</label>
-                                    <Input />
-                                </div>
-                                <div className="input-add-product">
-                                    <label>ราคา</label>
-                                    <Input />
-                                </div>
-                            </div>
-
-                            <div className="page-add-product">
-                                <div className="input-add-product">
-                                    <label>หมวดหมู่</label>
-                                    <select>
-                                        <option></option>
-                                        <option>เสื้อ</option>
-                                        <option>เครื่องประดับ</option>
-                                    </select>
-                                </div>
-                                <div className="input-add-product">
-                                    <label>แบรนด์</label>
-                                    <select>
-                                        <option></option>
-                                        <option>jone500</option>
-                                        <option>pbg</option>
-                                    </select>
-                                </div>
-                                <div className="input-add-product">
-                                    <label>คำอธิบาย</label>
-                                    <Input />
-                                </div>
-                            </div>
-
-                            <div className="page-add-product">
-                                <div className="input-add-product">
-                                    <InputImage />
-                                </div> 
-                            </div>
-
-                        </div>
-
-                        <div className="Product-add-button-group">
-                            <a className="Product-add-button-cancel" onClick={() => {setShowAdd(false);}}>ยกเลิก</a>
-                            <button><AiIcons.AiOutlinePlusCircle />เพิ่มสินค้า</button>
-                        </div>
-                    </Form>
-                </div>
-            }
-            
         
 
             {/*Delete modal*/}
-            <div id="Modal-Delete-Cate">
+            <div id="Modal-Delete-Cate" className="Modal-Delete-Cate">
                 <div className="Modal-Delete-Cate-body">
                     <h4>คุณต้องการจะลบสินค้าใช่หรือไม่</h4>
                     <div className="button-Cate-group-Delete">

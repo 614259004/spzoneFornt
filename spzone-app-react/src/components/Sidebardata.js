901918291a2351de2b243fce83c3949b052e121c
chanakan <614259008@webmail.npru.ac.th>
2021-09-23 00:52:18 +0700

    sidebar Category

--- spzone-app-react/src/components/Sidebardata.js
+++ spzone-app-react/src/components/Sidebardata.js
@@ -4,31 +4,37 @@ export const SidebarData = [
     {
         title: 'HOME',
         path: '/',
-        cName: 'nav-text'
+        cName: 'nav-text',
+        cStatus:1
     },
     {
         title: 'CATEGORY',
-        path: '/Category',
-        cName: 'nav-text'
+        path: '/Home/Category',
+        cName: 'nav-text',
+        cStatus:2
     },
     {
         title: 'BRAND',
         path: '/Home/Brand',
-        cName: 'nav-text'
+        cName: 'nav-text',
+        cStatus:1
     },
     {
         title: 'PRODUCT',
         path: '/Home/Product',
-        cName: 'nav-text'
+        cName: 'nav-text',
+        cStatus:1
     },
     {
         title: 'PRE ORDER',
         path: '/Home/Preorder',
-        cName: 'nav-text'
+        cName: 'nav-text',
+        cStatus:1
     },
     {
         title: 'PROMOTION',
         path: '/Home/Promotion',
-        cName: 'nav-text'
+        cName: 'nav-text',
+        cStatus:1
     }
 ]
